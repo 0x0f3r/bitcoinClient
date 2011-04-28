b599b16126fc8ee296b59801cf5b4b470f359150
Escalion <ryan@versobit.com>
2011-04-28 03:01:53 -0700

    converted test.php into a very simple plain-text api

--- test.php
+++ test.php
@@ -1,13 +1,31 @@
 <?php
+header('Content-type: application/txt');
 require_once('bitcoinClient.php');
 $bitcoin = new bitcoinClient("http://root:root@localhost:8332");
-echo $bitcoin->miscInfo(numBlocks), "<br />";
-echo $bitcoin->miscInfo(numNodes), "<br />";
-echo $bitcoin->miscInfo(diff), "<br />";
-echo $bitcoin->accountInfo(balance, "bitcoinClient"), "<br />";
-foreach($bitcoin->accountInfo(address, "bitcoinClient", true) as $address)
-{
-	echo $address, "<br />";
-}
+if(isset($_GET['task'])){
+  switch($_GET['task']){
+     case numBlocks:
+      echo $bitcoin->miscInfo(numBlocks);
+      break;
 
+     case numNodes:
+      echo $bitcoin->miscInfo(numNodes);
+      break;
+
+     case diff:
+      echo $bitcoin->miscInfo(diff);
+      break;
+
+     case balance:
+      echo $bitcoin->accountInfo(balance, "bitcoinClient");
+      break;
+     
+     case listAccounts:
+      foreach($bitcoin->accountInfo(address, "bitcoinClient", true) as $address)
+      {
+	echo $address, ";";
+      }
+      break;
+  }
+}
 ?>
